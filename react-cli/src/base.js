ce3f57e49aaadabca0aafd23cf830da904deec99
MarcusGH <kratzmarc@gmail.com>
2019-09-30 09:33:17 +0200

    react-cli

---
+++ react-cli/src/base.js
@@ -0,0 +1,12 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+// copier les 3 premières lignes de VOTRE config de votre base !!!
+ // NE PRENEZ PAS LES VALEURS QUI SONT ICI !!!
+    apiKey: " AIzaSyDov5IEGhTUFHgsAU3H9vuuDV31mpFEZNg",
+    authDomain: "react-intense-hobbies-70883.firebaseapp.com",
+    databaseURL: "https://react-intense-hobbies-70883.firebaseio.com/" // pas de , à la fin
+});
+
+
+export default base;
---
+++ react-cli/src/components/Username.js
@@ -0,0 +1,7 @@
+import React,{ Component } from 'react';
+
+function Username(props) {
+    return <h1>Hello, {props.name}</h1>;
+}
+
+export default Username
